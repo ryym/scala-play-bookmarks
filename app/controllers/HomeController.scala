d5308adb11bb51c68c0f4b42feab56cbba788292
ryym <ryym.64@gmail.com>
2017-12-23 22:12:27 +0900

    Load and display user

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,22 +3,17 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import scalikejdbc._
+import services.UserService
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(
+  cc: ControllerComponents,
+  userS: UserService,
+) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    val user = userS.currentUser
+    Ok(views.html.index(user.name))
   }
 }
