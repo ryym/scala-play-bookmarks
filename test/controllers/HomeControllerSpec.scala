b3570ed09fc89c767a757db5730c70132615978c
ryym <ryym.64@gmail.com>
2017-12-24 19:56:18 +0900

    Update home controller test

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -4,6 +4,9 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import org.mockito.Mockito._
+import models._
+import services._
 
 /**
  * Add your spec here.
@@ -16,30 +19,22 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
   "HomeController GET" should {
 
     "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
+      val userS = mock(classOf[UserService])
+      when(userS.currentUser).thenReturn(User(1, "test"))
+
+      val bookmarkS = mock(classOf[BookmarkService])
+      when(bookmarkS.listBookmarks(1)).thenReturn(Nil)
+
+      val controller = new HomeController(
+        stubControllerComponents(),
+        userS,
+        bookmarkS
+      )
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the application" in {
-      val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
-      val home = route(app, request).get
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Hello, test")
     }
   }
 }
